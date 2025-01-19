d4b32d1a22291c61606396f267413509c10bf1cf
Ayman <aymanerrarhiche@gmail.com>
2025-01-19 15:05:06 -0400

    cors

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
 from app import create_app
+from flask_cors import CORS
 
 load_dotenv()
 app = create_app()
+CORS(app)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8080)
